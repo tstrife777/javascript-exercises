4d98d8d86787643bdd4fcaaa4bd98067dc8c4340
Travis Moore <travis@mooremediallc.com>
2022-06-02 21:24:33 -0700

    Finish remaining lessons starting at 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let words = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    words = words + string;
+  }
+  return words;
 };
+console.log(repeatString("", 10));
 
 // Do not edit below this line
 module.exports = repeatString;
