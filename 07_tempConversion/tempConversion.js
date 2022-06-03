fe813c7de123dbdfcf1491c3472f0a5a1575f6bb
Travis Moore <travis@mooremediallc.com>
2022-06-03 14:19:34 -0700

    Finish rest starting at 08_calculator, just completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  celsius = (fahrenheit - 32) / 1.8;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  fahrenheit = celsius * 1.8 + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
+
+//Fahrenheigh to Celsius
+console.log(ftoc(30));
+
+//Celsius to Fahrenheigh
+console.log(ctof(-10));
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
